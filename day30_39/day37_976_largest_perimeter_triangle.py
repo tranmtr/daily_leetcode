ed2a4b70417b511812f479a066446d15fe09160d
Duong Thai Tran <duongthaitran68@gmail.com>
2025-09-28 10:21:33 +0700

    day 37

---
+++ day30_39/day37_976_largest_perimeter_triangle.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        for i in range(n - 1, 1, -1):
+            if (nums[i - 2] + nums[i - 1] > nums[i]):
+                return nums[i - 2] + nums[i - 1] + nums[i]
+        return 0
