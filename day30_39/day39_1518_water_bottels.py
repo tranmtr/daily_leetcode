a9d9a2e625d9a53aa1ca457800c1625e0e6d48bd
Duong Thai Tran <duongthaitran68@gmail.com>
2025-10-01 08:38:56 +0700

    day 39

---
+++ day30_39/day39_1518_water_bottels.py
@@ -0,0 +1,7 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        total = numBottles
+        while (numBottles // numExchange != 0):
+            total += numBottles // numExchange
+            numBottles = numBottles // numExchange + numBottles % numExchange
+        return total
